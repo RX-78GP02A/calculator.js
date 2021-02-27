16b381e45685109bd6030fc7b42ddee4fd77e8f7
RX-78GP02A <mridley@knights.ucf.edu>
2021-02-27 17:58:19 -0500

    - updated index.html

--- index.js
+++ index.js
@@ -2,3 +2,15 @@ const remove = document.querySelector('.remove');
 const result = document.querySelector('.screen');
 const operators = document.querySelectorAll('.keys');
 const numbers = document.getElementsByClassName('number');
+
+const displayNumbers = () => {
+    for (i = 0; i < numbers.length; i++) {
+        numbers[i].addEventListener('click', e => {
+            let value = result.innerHTML;
+            if (result.innerHTML === '0') {
+                result.innerHTML = ''
+            }
+            result.innerHTML += e.target.innerHTML;
+        })
+    }
+}
