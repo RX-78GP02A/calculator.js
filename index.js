69f96f1be586387a4806d2ce7244032724674733
RX-78GP02A <mridley@knights.ucf.edu>
2021-02-27 17:57:42 -0500

    - updated index.html

---
+++ index.js
@@ -0,0 +1,4 @@
+const remove = document.querySelector('.remove');
+const result = document.querySelector('.screen');
+const operators = document.querySelectorAll('.keys');
+const numbers = document.getElementsByClassName('number');
